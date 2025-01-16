4afceac305e59f32d86d64c27eb146be99802400
cyz-ing <1316769374@qq.com>
2025-01-16 02:58:05 +0000

    finish clippy3

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -9,16 +9,12 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if let Some(x)=my_option {
-        println!("my_option has a value");
-    }
-    else
-    {
-        println!("my_option is a None");
+    if my_option.is_none() {
+        println!("{:?}", my_option);
     }
     let my_arr = &[
         -1, -2, -3,
-        -4, -5, -6
+        -4, -5, -6,
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
