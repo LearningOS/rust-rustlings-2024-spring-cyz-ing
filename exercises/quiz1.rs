1d94e5d890dac01e0fb5e1327e72221ea11f0d9b
cyz-ing <1316769374@qq.com>
2025-01-19 01:40:15 +0000

    finish quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -19,6 +19,14 @@
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
+fn calculate_price_of_apples(number:i32) -> i32 {
+    if number <= 40 {
+        2 * number
+    } else {
+        1 * number
+    }
+}
+
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
