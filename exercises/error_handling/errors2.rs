b8886fe8674c5e4586ab89418731e31dc13f1016
cyz-ing <1316769374@qq.com>
2025-01-26 07:35:21 +0000

    finish errors2

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -27,8 +27,13 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
-
-    Ok(qty * cost_per_item + processing_fee)
+    match qty {
+        Ok(quantity) => {
+            let total_cost = quantity * cost_per_item + processing_fee;
+            Ok(total_cost)
+        }
+        Err(err) => Err(err),
+    }
 }
 
 #[cfg(test)]
