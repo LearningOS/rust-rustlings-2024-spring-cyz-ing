1e32a42dd548f0ec7ea7b43b56aabab977353a25
cyz-ing <1316769374@qq.com>
2025-01-27 08:54:50 +0000

    finish errors3

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -11,7 +11,7 @@
 
 use std::num::ParseIntError;
 
-fn main() {
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
@@ -23,12 +23,20 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>()?;
+    let qty = item_quantity.parse::<i32>();
+    match qty{
+        Ok(quantity) => {
+            let total_cost=(quantity * cost_per_item + processing_fee);
+            Ok(total_cost)
+        }
+        Err(err)=>Err(err)
 
-    Ok(qty * cost_per_item + processing_fee)
+    }
+
 }
