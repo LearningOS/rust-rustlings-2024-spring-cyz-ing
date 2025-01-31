e3c3bd1c771fc2a7d2ad306d39d1950432c61543
cyz-ing <1316769374@qq.com>
2025-01-31 05:41:36 +0000

    finish macros4

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -9,13 +9,14 @@
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
 
 fn main() {
     my_macro!();
-    my_macro!(7777);
+    let x="7777";
+    my_macro!(x);
 }
