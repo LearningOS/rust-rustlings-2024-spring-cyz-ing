870837d16a830724ab3c61cb3d3368dbe8d1d0c0
cyz-ing <1316769374@qq.com>
2025-02-13 03:04:00 +0000

    finish algorithn5

--- exercises/algorithm/algorithm5.rs
+++ exercises/algorithm/algorithm5.rs
@@ -27,13 +27,33 @@ impl Graph {
 
     // Perform a breadth-first search on the graph, return the order of visited nodes
     fn bfs_with_return(&self, start: usize) -> Vec<usize> {
-        
-		//TODO
-
         let mut visit_order = vec![];
+        let mut visited = vec![false; self.adj.len()];
+        let mut queue = VecDeque::new();
+
+        // Mark the start node as visited and enqueue it
+        visited[start] = true;
+        queue.push_back(start);
+
+        while let Some(node) = queue.pop_front() {
+            // Add the node to the visit order
+            visit_order.push(node);
+
+            // Get all adjacent vertices of the dequeued vertex
+            for &neighbor in &self.adj[node] {
+                if!visited[neighbor] {
+                    // Mark the neighbor as visited and enqueue it
+                    visited[neighbor] = true;
+                    queue.push_back(neighbor);
+                }
+            }
+        }
+
         visit_order
     }
+
 }
+
 
 
 #[cfg(test)]
