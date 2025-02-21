00ca68c6a5a1af11e17c5fb5cd87f2d8d22e03a0
cyz-ing <1316769374@qq.com>
2025-02-21 02:50:50 +0000

    finish algorithm10

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -1,48 +1,73 @@
-/*
-	graph
-	This problem requires you to implement a basic graph functio
-*/
-// I AM NOT DONE
-
 use std::collections::{HashMap, HashSet};
 use std::fmt;
+
 #[derive(Debug, Clone)]
 pub struct NodeNotInGraph;
+
 impl fmt::Display for NodeNotInGraph {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "accessing a node that is not in the graph")
     }
 }
+
 pub struct UndirectedGraph {
     adjacency_table: HashMap<String, Vec<(String, i32)>>,
 }
+
 impl Graph for UndirectedGraph {
     fn new() -> UndirectedGraph {
         UndirectedGraph {
             adjacency_table: HashMap::new(),
         }
     }
+
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
         &mut self.adjacency_table
     }
+
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>> {
         &self.adjacency_table
     }
+
+    fn add_node(&mut self, node: &str) -> bool {
+        let node_str = node.to_string();
+        if self.contains(node) {
+            false
+        } else {
+            self.adjacency_table.insert(node_str, Vec::new());
+            true
+        }
+    }
+
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let (from, to, weight) = edge;
+        let from_str = from.to_string();
+        let to_str = to.to_string();
+
+        // 确保两个节点都存在于图中
+        self.add_node(from);
+        self.add_node(to);
+
+        // 在 from 节点的邻居列表中添加 to 节点及其权重
+        let from_neighbors = self.adjacency_table.get_mut(&from_str).unwrap();
+        if!from_neighbors.iter().any(|(n, _)| *n == to_str) {
+            from_neighbors.push((to_str.clone(), weight));
+        }
+
+        // 在 to 节点的邻居列表中添加 from 节点及其权重
+        let to_neighbors = self.adjacency_table.get_mut(&to_str).unwrap();
+        if!to_neighbors.iter().any(|(n, _)| *n == from_str) {
+            to_neighbors.push((from_str, weight));
+        }
     }
 }
+
 pub trait Graph {
     fn new() -> Self;
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
-    fn add_node(&mut self, node: &str) -> bool {
-        //TODO
-		true
-    }
-    fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
-    }
+    fn add_node(&mut self, node: &str) -> bool;
+    fn add_edge(&mut self, edge: (&str, &str, i32));
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
     }
@@ -59,16 +84,19 @@ pub trait Graph {
         edges
     }
 }
+
 #[cfg(test)]
 mod test_undirected_graph {
     use super::Graph;
     use super::UndirectedGraph;
+
     #[test]
     fn test_add_edge() {
         let mut graph = UndirectedGraph::new();
         graph.add_edge(("a", "b", 5));
         graph.add_edge(("b", "c", 10));
         graph.add_edge(("c", "a", 7));
+
         let expected_edges = [
             (&String::from("a"), &String::from("b"), 5),
             (&String::from("b"), &String::from("a"), 5),
@@ -77,6 +105,7 @@ mod test_undirected_graph {
             (&String::from("b"), &String::from("c"), 10),
             (&String::from("c"), &String::from("b"), 10),
         ];
+
         for edge in expected_edges.iter() {
             assert_eq!(graph.edges().contains(edge), true);
         }
