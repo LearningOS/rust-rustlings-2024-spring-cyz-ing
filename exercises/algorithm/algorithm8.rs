d98c0fc2c0b6d3be8c200404bfe69e99de2b5fd2
cyz-ing <1316769374@qq.com>
2025-02-24 04:20:34 +0000

    finish algorithm8

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -68,14 +68,34 @@ impl<T> myStack<T> {
     }
     pub fn push(&mut self, elem: T) {
         //TODO
+        if self.q1.is_empty() {
+            self.q2.enqueue(elem);
+        } else {
+            self.q1.enqueue(elem);
+        }
     }
     pub fn pop(&mut self) -> Result<T, &str> {
         //TODO
-		Err("Stack is empty")
+        if self.q1.is_empty() && self.q2.is_empty() {
+            return Err("Stack is empty");
+        }
+
+        let (mut non_empty_queue, mut empty_queue) = if self.q1.is_empty() {
+            (&mut self.q2, &mut self.q1)
+        } else {
+            (&mut self.q1, &mut self.q2)
+        };
+
+        while non_empty_queue.size() > 1 {
+            if let Ok(elem) = non_empty_queue.dequeue() {
+                empty_queue.enqueue(elem);
+            }
+        }
+        non_empty_queue.dequeue()
     }
     pub fn is_empty(&self) -> bool {
 		//TODO
-        true
+        self.q1.elements.is_empty() && self.q2.elements.is_empty()
     }
 }
 
